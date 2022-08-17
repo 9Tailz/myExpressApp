f9fe6e5ec24b51178244dba6421b31a9279374d1
9Tailz <jordo-t@hotmail.com>
2022-08-17 13:02:23 +0000

    add js function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,11 @@
 console.log('Hellow')
 
-document.getElementById("myBtn").addEventListener("click",test)
+document.getElementById("myBtn").addEventListener("click",test2)
 
 function test(){
     console.log('click');
+}
+
+function test2(){
+    console.log('clickID');
 }
